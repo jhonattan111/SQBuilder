244098f7d054b60ab7acd2c5b09c51a65609ccae
jhonattan111 <jhonattan10alves@gmail.com>
2022-12-08 22:37:28 -0300

    SB-0001 updating package to version 2.0

--- SQBuilder/EDatabases.cs
+++ SQBuilder/EDatabases.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SQBuilder
+﻿namespace SQBuilder
 {
-    internal enum EDatabases : int
+    public enum EDatabases : int
     {
         SQLServer = 0,
         PostgreeSQL = 1,
         MySQL = 2,
         Sybase = 3,
-        Oracle = 4,
     }
 }
